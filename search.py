166dbfa52367ef89b5fde1a3dd5f0386032b70d2
warayutnotebook <wadisterz@gmail.com>
2023-05-09 00:38:50 +0700

    done

--- search.py
+++ search.py
@@ -16,6 +16,19 @@ class SearchEngine:
         results = [(i, score) for i, score in enumerate(scores)]
         results = sorted(results, key=lambda x: -x[1])
         return results
+    # def __init__(self, documents):
+    #     self.documents = documents
+    #     self.vectorizer = TfidfVectorizer()
+    #     self.index = self.vectorizer.fit_transform([doc['text'] for doc in documents])
+
+    # def search(self, query):
+    #     query_vector = self.vectorizer.transform([query])
+    #     scores = cosine_similarity(query_vector, self.index)[0]
+    #     results = [(i, score) for i, score in enumerate(scores)]
+    #     results = sorted(results, key=lambda x: -x[1])
+    #     return [(self.documents[i]['url'], score) for i, score in results]
+
+
 
 documents = [
     """มาริโอ้" โชว์หวาน "จันจิ" คือ คน เดียว ใน ใจ รัก 8 ปี จับมือ แฟน ก้าวผ่าน คำ บูลลี่""",
@@ -25,16 +38,40 @@ documents = [
     "ประวัติ ไอซ์ ปรีชญา พงษ์ธนานิกร"
 ]
 
+# documents = [
+#     {
+#         "text": """มาริโอ้" โชว์หวาน "จันจิ" คือ คน เดียว ใน ใจ รัก 8 ปี จับมือ แฟน ก้าวผ่าน คำ บูลลี่""",
+#         "url": "https://example.com/document1"
+#     },
+#     {
+#         "text": "ไอซ์ ปรีชญา เคลียร์ ปม สั่ง ไซยาไนด์ กำจัด สัตว์ มี พิษ ร่ำไห้ ถูกโยงอดีต ผู้ จัดการ เสียชีวิต",
+#         "url": "https://example.com/document2"
+#     },
+#     {
+#         "text": "“มาริโอ้” ไม่ ซีเรียส แต่ เสียใจ ถูก ตั้งกระทู้ วิจารณ์ รูปร่าง",
+#         "url": "https://example.com/document3"
+#     },
+#     {
+#         "text": "มาริโอ้ วิ่งไปหา จันจิ ถ่ายรูป ด้วยกัน สุด มุ้งมิ้ง เสิร์ฟ โมเมนต์ หวาน หาดูยาก",
+#         "url": "https://example.com/document4"
+#     },
+#     {
+#         "text": "ประวัติ ไอซ์ ปรีชญา พงษ์ธนานิกร",
+#         "url": "https://example.com/document5"
+#     }
+# ]
+
+
 engine = SearchEngine(documents)
 
 @app.route('/')
 def index():
-    return render_template('search.html')
+    return render_template('index.html')
 
 @app.route('/search')
 def search():
     query = request.args.get('query')
-    print('query = ',query)
+    print('query = ',query.encode("utf-8"))
     results = engine.search(query)
     print('result = ',results)
     return render_template('results.html', query=query, results=results, documents=documents)
